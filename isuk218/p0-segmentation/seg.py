7c9222d6615659a476e75cbda3d7aa583fa134b8
Syamira <isuk218@aucklanduni.ac.nz>
2018-06-21 00:27:22 +1200

    need to test with a different BW image

--- isuk218/p0-segmentation/seg.py
+++ isuk218/p0-segmentation/seg.py
@@ -1,6 +1,7 @@
 #load the image
 from skimage import io as skio
-url = 'http://i.stack.imgur.com/SYxmp.jpg'
+#url = 'http://i.stack.imgur.com/SYxmp.jpg'
+url = 'https://images.unsplash.com/photo-1508273838848-8d6f5162b102?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d3fe6235fa5000f4acbe827b83ddeaab&auto=format&fit=crop&w=500&q=60'
 img = skio.imread(url)
 
 print("shape of image: {}".format(img.shape))
@@ -11,7 +12,7 @@ from skimage import filters
 sobel = filters.sobel(img)
 
 import matplotlib.pyplot as plt
-%matplotlib inline
+#matplotlib inline
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 plt.rcParams['figure.dpi'] = 200
@@ -63,3 +64,45 @@ plt.imshow(cleaned)
 
 plt.imshow(cleaned, cmap='gray') #apply a red color to the background to check
 plt.imshow(background_mask.reshape(background_mask.shape + (1,)) * np.array([1, 0, 0, 1]))
+
+#picking another group
+def draw_group_as_background(ax, group, watershed_result, original_image):
+    "Draws a group from the watershed result as red background."
+    background_mask = (watershed_result == group)
+    cleaned = original_image * ~background_mask
+    ax.imshow(cleaned, cmap='gray')
+    ax.imshow(background_mask.reshape(background_mask.shape + (1,)) * np.array([1, 0, 0, 1]))
+  
+background_candidates = sorted(set(ws.ravel()), key=lambda g: np.sum(ws == g), reverse=True)
+
+N = 3
+fig, axes = plt.subplots(N, N, figsize=(6, 8))
+for i in range(N*N):
+    draw_group_as_background(axes.ravel()[i], background_candidates[i], ws, img)
+plt.tight_layout()
+ 
+#manually inputting seeds
+seed_mask = np.zeros(img.shape, dtype=np.int)
+seed_mask[0, 0] = 1 # background
+seed_mask[600, 400] = 2 # foreground
+
+ws = morphology.watershed(blurred, seed_mask)
+plt.imshow(ws)
+
+fig, ax = plt.subplots()
+draw_group_as_background(ax, 1, ws, img)
+
+seed_mask = np.zeros(img.shape, dtype=np.int)
+seed_mask[0, 0] = 1 # background
+seed_mask[600, 400] = 2 # foreground
+seed_mask[1000, 150] = 2 # left arm
+
+ws = morphology.watershed(blurred, seed_mask)
+plt.imshow(ws)
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(img)
+draw_group_as_background(ax[1], 1, ws, img)
+
+    
+plt.show()
