352f80f9d793af0e9283fc39972db37047db85c3
Syamira <isuk218@aucklanduni.ac.nz>
2018-06-23 05:05:32 +1200

    accepts 3d coloured image for both sobel and gaussian filter

--- isuk218/p0-segmentation/seg.py
+++ isuk218/p0-segmentation/seg.py
@@ -1,33 +1,45 @@
 from skimage import io as skio
+from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
 from skimage import filters
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy import ndimage as ndi
 from skimage import morphology
+from skimage.color import grey2rgb
+from skimage.color import rgb2gray
 
 #step 1: load the image
-url = 'http://i.stack.imgur.com/SYxmp.jpg'
-#url = 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Nine_steps_stair_in_Lysekil_-_bw.jpg'
-#img = skio.imread(url)
+print("-LOAD IMAGE-----------------------------")
 img = scipy.ndimage.imread('SYxmp.jpg')
 print("shape of image: {}".format(img.shape))
-print("dtype of image: {}".format(img.dtype))
+print("dtype of image: {}".format(img.dtype)) #type is uint8 for 8-bit images (0-255)
+print("----------------------------------------")
+
 
 #step 2: detect edges
-sobel = filters.sobel(img)
+@adapt_rgb(each_channel)
+def sobel_each(image):
+    return filters.sobel(image)
 
-#step 3: matplotlib inline
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-plt.rcParams['figure.dpi'] = 200
+@adapt_rgb(each_channel)
+def gaussian_each(image):
+    return filters.gaussian(image, sigma=2.0)
 
+sobel = sobel_each(img)
+blurred = gaussian_each(sobel)
+
+#step 3: matplotlib settings
+plt.rcParams['image.interpolation'] = 'nearest' #display image without trying to interpolate between pixels if the display resolution is not the same as the image resolution
+plt.rcParams['figure.dpi'] = 80 #set size of the image displayed
 plt.imshow(sobel)
 
-blurred = filters.gaussian(sobel, sigma=2.0)
+
 plt.imshow(blurred) #blur this image a bit to make the edges thicker
 
+
 #step 4: obtaining seeds for the watershed transform
+print("-WATERSHED------------------------------")
 light_spots = np.array((img > 245).nonzero()).T
 
 print("shape of light_spots: {}".format(light_spots.shape))
@@ -36,12 +48,15 @@ plt.plot(light_spots[:, 1], light_spots[:, 0], 'o')
 plt.imshow(img)
 plt.title('light spots in image')
 
+
 dark_spots = np.array((img < 3).nonzero()).T
 print("shape of light_spots: {}".format(dark_spots.shape))
 
 plt.plot(dark_spots[:, 1], dark_spots[:, 0], 'o')
 plt.imshow(img)
 plt.title('dark spots in image')
+
+print("----------------------------------------")
 
 #step 5: making a seed mask
 bool_mask = np.zeros(img.shape, dtype=np.bool)
@@ -63,6 +78,7 @@ plt.imshow(~background_mask)
 
 cleaned = img * ~background_mask
 plt.imshow(cleaned)
+
 
 plt.imshow(cleaned, cmap='gray') #apply a red color to the background to check
 plt.imshow(background_mask.reshape(background_mask.shape + (1,)) * np.array([1, 0, 0, 1]))
@@ -106,5 +122,5 @@ fig, ax = plt.subplots(1, 2)
 ax[0].imshow(img)
 draw_group_as_background(ax[1], 1, ws, img)
 
-    
 plt.show()
+
