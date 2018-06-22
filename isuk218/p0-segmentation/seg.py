dbdbf056da41f12a2b4c5281cd021ab159fa10c7
Syamira <isuk218@aucklanduni.ac.nz>
2018-06-22 21:27:50 +1200

    reads image from file instead of url

--- isuk218/p0-segmentation/seg.py
+++ isuk218/p0-segmentation/seg.py
@@ -1,18 +1,23 @@
-#load the image
 from skimage import io as skio
-#url = 'http://i.stack.imgur.com/SYxmp.jpg'
-url = 'https://images.unsplash.com/photo-1508273838848-8d6f5162b102?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d3fe6235fa5000f4acbe827b83ddeaab&auto=format&fit=crop&w=500&q=60'
-img = skio.imread(url)
+from skimage import filters
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from scipy import ndimage as ndi
+from skimage import morphology
 
+#step 1: load the image
+url = 'http://i.stack.imgur.com/SYxmp.jpg'
+#url = 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Nine_steps_stair_in_Lysekil_-_bw.jpg'
+#img = skio.imread(url)
+img = scipy.ndimage.imread('SYxmp.jpg')
 print("shape of image: {}".format(img.shape))
 print("dtype of image: {}".format(img.dtype))
 
-#detect edges
-from skimage import filters
+#step 2: detect edges
 sobel = filters.sobel(img)
 
-import matplotlib.pyplot as plt
-#matplotlib inline
+#step 3: matplotlib inline
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 plt.rcParams['figure.dpi'] = 200
@@ -22,33 +27,30 @@ plt.imshow(sobel)
 blurred = filters.gaussian(sobel, sigma=2.0)
 plt.imshow(blurred) #blur this image a bit to make the edges thicker
 
-#obtaining seeds for the watershed transform
-import numpy as np
+#step 4: obtaining seeds for the watershed transform
 light_spots = np.array((img > 245).nonzero()).T
 
-light_spots.shape
+print("shape of light_spots: {}".format(light_spots.shape))
 
 plt.plot(light_spots[:, 1], light_spots[:, 0], 'o')
 plt.imshow(img)
 plt.title('light spots in image')
 
 dark_spots = np.array((img < 3).nonzero()).T
-dark_spots.shape
+print("shape of light_spots: {}".format(dark_spots.shape))
 
 plt.plot(dark_spots[:, 1], dark_spots[:, 0], 'o')
 plt.imshow(img)
 plt.title('dark spots in image')
 
-#making a seed mask
-from scipy import ndimage as ndi
+#step 5: making a seed mask
 bool_mask = np.zeros(img.shape, dtype=np.bool)
 bool_mask[tuple(light_spots.T)] = True
 bool_mask[tuple(dark_spots.T)] = True
 seed_mask, num_seeds = ndi.label(bool_mask)
 num_seeds
 
-#applying the watershed
-from skimage import morphology
+#step 6: applying the watershed
 ws = morphology.watershed(blurred, seed_mask)
 plt.imshow(ws)
 
@@ -65,7 +67,7 @@ plt.imshow(cleaned)
 plt.imshow(cleaned, cmap='gray') #apply a red color to the background to check
 plt.imshow(background_mask.reshape(background_mask.shape + (1,)) * np.array([1, 0, 0, 1]))
 
-#picking another group
+#step 7: picking another group
 def draw_group_as_background(ax, group, watershed_result, original_image):
     "Draws a group from the watershed result as red background."
     background_mask = (watershed_result == group)
@@ -81,7 +83,7 @@ for i in range(N*N):
     draw_group_as_background(axes.ravel()[i], background_candidates[i], ws, img)
 plt.tight_layout()
  
-#manually inputting seeds
+#step 8: manually inputting seeds
 seed_mask = np.zeros(img.shape, dtype=np.int)
 seed_mask[0, 0] = 1 # background
 seed_mask[600, 400] = 2 # foreground
