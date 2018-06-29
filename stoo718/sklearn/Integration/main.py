0962ff2490802a274051ea0f49c2ab7c46995bda
Serena Toon <stoo718@aucklanduni.ac.nz>
2018-06-29 20:53:29 +1200

    integrate web search

--- stoo718/sklearn/Integration/main.py
+++ stoo718/sklearn/Integration/main.py
@@ -1,6 +1,7 @@
 from extract_sub_image import get_sub_image
 from colour_identification import identify_colour
 from predict import get_prediction
+from amazon_search import do_search
 
 input_filename = 'j6885a-classic-leather-jacket-blk-leather-b.jpg'
 
@@ -14,3 +15,10 @@ print colour
 #step 3: get material
 material = get_prediction('leather.png')
 print material
+
+# step 4: get type of clothing
+
+# step 5: web search
+query = colour + " " + material + " jacket"
+print query
+do_search(query)
