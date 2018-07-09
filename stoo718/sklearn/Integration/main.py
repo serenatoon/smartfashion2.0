47f48c0cf6306402b885063c486d7df07677d87d
Serena Toon <stoo718@aucklanduni.ac.nz>
2018-07-09 20:16:42 +1200

    integrated bg removal yay

--- stoo718/sklearn/Integration/main.py
+++ stoo718/sklearn/Integration/main.py
@@ -2,26 +2,32 @@ from extract_sub_image import get_sub_image
 from colour_identification import identify_colour
 from predict import get_prediction
 from amazon_search import do_search
+from slic_dir import remove_background
 
-input_filename = 'removedbg.png'
+input_filename = 'leatherjacket.jpg'
 
-# step 1: extract sub-image
-sub_img = get_sub_image(input_filename)
+# step 1: remove background
+remove_background(input_filename)
+# output will be saved to removed_bg.png
 
-# step 2: identify colour
+# step 2: extract sub-image
+sub_img = get_sub_image('removed_bg.png')
+
+# step 3: identify colour
 #colour = identify_colour('sharpened.png')
-colour = identify_colour(input_filename)
+#colour = identify_colour('removed_bg.png')
+colour = identify_colour('cropped.png')
 print colour
 
-#step 3: get material
+#step 4: get material
 material = get_prediction('cropped.png', "material")
 print material
 
-# step 4: get type of clothing
-clothing_type = get_prediction(input_filename, "clothing_type")
+# step 5: get type of clothing
+clothing_type = get_prediction('removed_bg.png', "clothing_type")
 print clothing_type
 
-# step 5: web search
-query = colour + " " + material + " jacket"
+# step 6: web search
+query = colour + " " + material + " " + clothing_type
 print query
 do_search(query)
